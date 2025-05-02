78287b69aa912259f9b3b4677c9b690344bd510c
isthatpratik <pratik.desai18.pd@gmail.com>
2025-05-02 20:03:18 +0530

    Minor Changes

--- get-google-refresh-token.js
+++ get-google-refresh-token.js
@@ -1,9 +1,9 @@
 const { google } = require('googleapis');
 const readline = require('readline');
 
-const CLIENT_ID = '837161369193-q4kn6oi82t23mbb91lpgol0n8hpffq54.apps.googleusercontent.com'; // <-- Paste your client ID here
-const CLIENT_SECRET = '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'; // <-- Paste your client secret here
-const REDIRECT_URI = 'http://localhost:3000/api/oauth2callback'; // Or your deployed callback
+const CLIENT_ID = ''; // <-- Paste your client ID here
+const CLIENT_SECRET = ''; // <-- Paste your client secret here
+const REDIRECT_URI = ''; // Or your deployed callback
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
